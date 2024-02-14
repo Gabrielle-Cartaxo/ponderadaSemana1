2141428dd9958978eadfbd2ddf2a1533c3eb9a52
Gabrielle-Cartaxo <gabrielle.cartaxo@sou.inteli.edu.br>
2024-02-14 20:54:59 -0300

    correção de caminho pt5

--- ponderada/docusaurus.config.js
+++ ponderada/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://gabrielle-cartaxo.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ponderadaSemana 1/',
+  baseUrl: '/ponderadaSemana1/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
